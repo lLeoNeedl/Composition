382578e712d19c7add884d2f0e1af88c54310dcf
Leonid Lavrov <274852@gmail.com>
2023-01-17 15:33:48 +0200

    Adding Parcelables

--- app/src/main/java/com/example/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/example/composition/presentation/GameFinishedFragment.kt
@@ -42,6 +42,9 @@ class GameFinishedFragment : Fragment() {
                     retryGame()
                 }
             })
+        binding.buttonRetry.setOnClickListener {
+            retryGame()
+        }
     }
 
     override fun onDestroyView() {
@@ -50,7 +53,9 @@ class GameFinishedFragment : Fragment() {
     }
 
     private fun parseArgs() {
-        gameResult = requireArguments().getSerializable(GAME_RESULT_KEY) as GameResult
+        requireArguments().getParcelable<GameResult>(GAME_RESULT_KEY)?.let {
+            gameResult = it
+        }
     }
 
     private fun retryGame() {
@@ -67,7 +72,7 @@ class GameFinishedFragment : Fragment() {
         fun newInstance(gameResult: GameResult) =
             GameFinishedFragment().apply {
                 arguments = Bundle().apply {
-                    putSerializable(GAME_RESULT_KEY, gameResult)
+                    putParcelable(GAME_RESULT_KEY, gameResult)
                 }
             }
     }
