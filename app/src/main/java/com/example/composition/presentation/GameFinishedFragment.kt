4fa7cb4d976a65ce156c1f62f8af9521e19ee8ae
Leonid Lavrov <274852@gmail.com>
2023-01-20 18:39:30 +0200

    Adding Data Binding to GameFinishedFragment.kt

--- app/src/main/java/com/example/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/example/composition/presentation/GameFinishedFragment.kt
@@ -30,7 +30,7 @@ class GameFinishedFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setClickListeners()
-        setViews()
+        binding.gameResult = args.gameResult
     }
 
     override fun onDestroyView() {
@@ -41,41 +41,6 @@ class GameFinishedFragment : Fragment() {
     private fun setClickListeners() {
         binding.buttonRetry.setOnClickListener {
             retryGame()
-        }
-    }
-
-    private fun setViews() {
-        binding.gameResult = args.gameResult
-        if (args.gameResult.winner) {
-            binding.emojiResult.setImageResource(R.drawable.ic_smile)
-        } else {
-            binding.emojiResult.setImageResource(R.drawable.ic_sad)
-        }
-        with(binding) {
-//            tvRequiredAnswers.text = String.format(
-//                resources.getString(R.string.required_score),
-//                gameResult.gameSettings.minCountOfRightAnswers
-//            )
-//            tvRequiredPercentage.text = String.format(
-//                resources.getString(R.string.required_percentage),
-//                gameResult.gameSettings.minPercentOfRightAnswers
-//            )
-//            tvScoreAnswers.text = String.format(
-//                resources.getString(R.string.score_answers),
-//                gameResult.countOfRightAnswers
-//            )
-            tvScorePercentage.text = String.format(
-                resources.getString(R.string.score_percentage),
-                calculatePercentOfRightAnswers()
-            )
-        }
-    }
-
-    private fun calculatePercentOfRightAnswers(): Int {
-        return if (args.gameResult.countOfQuestions == 0) {
-            0
-        } else {
-            (args.gameResult.countOfRightAnswers / args.gameResult.countOfQuestions.toDouble() * 100).toInt()
         }
     }
 
